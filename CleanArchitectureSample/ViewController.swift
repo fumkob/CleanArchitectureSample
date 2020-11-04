00224406f92200df123d52363287010345ce6168
Fumiaki Kobayashi <fumiaki-kobayashi@mail.nissan.co.jp>
2020-11-04 16:36:03 +0900

    Initial Commit

---
+++ CleanArchitectureSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitectureSample
+//
+//  Created by Fumiaki Kobayashi on 2020/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
