00630dd1306c3d92ead6278a45fa7b45813609df
Fumiaki Kobayashi <fumiaki-kobayashi@mail.nissan.co.jp>
2020-11-05 12:32:36 +0900

    Setup Swiftgen

---
+++ CleanArchitectureSample/Generated/Storyboard Scenes.swift
@@ -0,0 +1,100 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+// swiftlint:disable sorted_imports
+import Foundation
+import UIKit
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length implicit_return
+
+// MARK: - Storyboard Scenes
+
+// swiftlint:disable explicit_type_interface identifier_name line_length type_body_length type_name
+internal enum StoryboardScene {
+  internal enum LaunchScreen: StoryboardType {
+    internal static let storyboardName = "LaunchScreen"
+
+    internal static let initialScene = InitialSceneType<UIKit.UIViewController>(storyboard: LaunchScreen.self)
+  }
+  internal enum LoginAccount: StoryboardType {
+    internal static let storyboardName = "LoginAccount"
+
+    internal static let loginAccount = SceneType<CleanArchitectureSample.LoginAccountViewController>(storyboard: LoginAccount.self, identifier: "loginAccount")
+  }
+  internal enum Root: StoryboardType {
+    internal static let storyboardName = "Root"
+
+    internal static let initialScene = InitialSceneType<CleanArchitectureSample.RootViewController>(storyboard: Root.self)
+
+    internal static let root = SceneType<CleanArchitectureSample.RootViewController>(storyboard: Root.self, identifier: "root")
+  }
+  internal enum Timeline: StoryboardType {
+    internal static let storyboardName = "Timeline"
+
+    internal static let timeline = SceneType<CleanArchitectureSample.TimelineViewController>(storyboard: Timeline.self, identifier: "timeline")
+  }
+}
+// swiftlint:enable explicit_type_interface identifier_name line_length type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal protocol StoryboardType {
+  static var storyboardName: String { get }
+}
+
+internal extension StoryboardType {
+  static var storyboard: UIStoryboard {
+    let name = self.storyboardName
+    return UIStoryboard(name: name, bundle: BundleToken.bundle)
+  }
+}
+
+internal struct SceneType<T: UIViewController> {
+  internal let storyboard: StoryboardType.Type
+  internal let identifier: String
+
+  internal func instantiate() -> T {
+    let identifier = self.identifier
+    guard let controller = storyboard.storyboard.instantiateViewController(withIdentifier: identifier) as? T else {
+      fatalError("ViewController '\(identifier)' is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+
+  @available(iOS 13.0, tvOS 13.0, *)
+  internal func instantiate(creator block: @escaping (NSCoder) -> T?) -> T {
+    return storyboard.storyboard.instantiateViewController(identifier: identifier, creator: block)
+  }
+}
+
+internal struct InitialSceneType<T: UIViewController> {
+  internal let storyboard: StoryboardType.Type
+
+  internal func instantiate() -> T {
+    guard let controller = storyboard.storyboard.instantiateInitialViewController() as? T else {
+      fatalError("ViewController is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+
+  @available(iOS 13.0, tvOS 13.0, *)
+  internal func instantiate(creator block: @escaping (NSCoder) -> T?) -> T {
+    guard let controller = storyboard.storyboard.instantiateInitialViewController(creator: block) else {
+      fatalError("Storyboard \(storyboard.storyboardName) does not have an initial scene.")
+    }
+    return controller
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
