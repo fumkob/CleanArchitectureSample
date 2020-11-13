e9309c7b516564a36e4bd3793ac115db71d88ad5
Fumiaki Kobayashi <fumiaki-kobayashi@mail.nissan.co.jp>
2020-11-13 18:51:23 +0900

    Implement TimelineEntity

--- CleanArchitectureSample/Data/Entity/TimelineEntity.swift
+++ CleanArchitectureSample/Data/Entity/TimelineEntity.swift
@@ -6,7 +6,38 @@
 //
 
 import Foundation
+import ObjectMapper
 
-public struct TimelineEntity {
+public struct TimelineEntity: Mappable {
+    var text = ""
+    var createdAt = ""
+    var user: UserEntity?
     
+    public init?(map: Map) {
+    }
+    
+    public mutating func mapping(map: Map) {
+        text <- map["text"]
+        createdAt <- map["createdAt"]
+        user <- map["user"]
+    }
+}
+
+public struct UserEntity: Mappable {
+    var screenName = ""
+    var userDescription = ""
+    var profileUrl = ""
+    var profileBackgroundUrl = ""
+    var name = ""
+    
+    public init?(map: Map) {
+    }
+    
+    public mutating func mapping(map: Map) {
+        screenName <- map["screenName"]
+        userDescription <- map["description"]
+        profileUrl <- map["profile_image_url_https"]
+        profileBackgroundUrl <- map["profile_background_image_url_https"]
+        name <- map["name"]
+    }
 }
